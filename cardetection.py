68c2bf115f26707935dc1cfea5419f10dce93d09
rock-far <80121339+rock-far@users.noreply.github.com>
2023-09-16 21:50:01 -0400

    Create cardetection.py

---
+++ cardetection.py
@@ -0,0 +1,22 @@
+import cv2
+
+cars_cascade = cv2.CascadeClassifier('haarcascade_car.xml')
+
+cap = cv2.VideoCapture('cars.mp4')#with preview video
+
+while True:
+    _, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    cars = cars_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for(x,y,w,h) in cars:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
